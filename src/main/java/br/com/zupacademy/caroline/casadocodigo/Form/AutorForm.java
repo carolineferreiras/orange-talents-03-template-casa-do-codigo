70e02b9f1bdf5e7e7f72dd235257e6be863fcc80
carolineferreiras <caroline.ferreira@zup.com.br>
2021-03-30 13:01:18 -0300

    atualização email

--- src/main/java/br/com/zupacademy/caroline/casadocodigo/Form/AutorForm.java
+++ src/main/java/br/com/zupacademy/caroline/casadocodigo/Form/AutorForm.java
@@ -3,6 +3,7 @@ package br.com.zupacademy.caroline.casadocodigo.Form;
 import br.com.zupacademy.caroline.casadocodigo.Models.Autor;
 import com.sun.istack.NotNull;
 
+import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -14,6 +15,7 @@ public class AutorForm {
     private String nome;
     @Email
     @NotNull @NotEmpty
+    @Column(unique = true)
     private String email;
     @NotNull @NotEmpty @Size(max=400)
     private String descricao;
@@ -22,4 +24,5 @@ public class AutorForm {
     public Autor converter(){
       return new Autor(nome,email,descricao);
     }
+
 }
